c3524f7e224e2ec17045a54a1b857064d5617971
Andres Fuentes <me@andresfuentes.com>
2016-08-31 12:27:28 +0000

    adding extra field

--- src/main/webapp/app/entities/checklist-question/checklist-question.state.js
+++ src/main/webapp/app/entities/checklist-question/checklist-question.state.js
@@ -125,6 +125,7 @@
                         entity: function () {
                             return {
                                 code: null,
+                                description: null,
                                 id: null
                             };
                         }
