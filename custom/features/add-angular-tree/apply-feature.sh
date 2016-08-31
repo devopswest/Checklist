aaa78845a0f0d529a1571bc4daf918f7b0c91422
Andres Fuentes <me@andresfuentes.com>
2016-08-31 20:41:48 +0000

    sp;lit in two features

--- custom/features/add-angular-tree/apply-feature.sh
+++ custom/features/add-angular-tree/apply-feature.sh
@@ -9,7 +9,3 @@ echo "**************************"
 # Add angular-tree-ui
 bower install angular-ui-tree --save
 sed -i "s/angular-loading-bar/angular-loading-bar',\n\t\t\t'ui.tree/" src/main/webapp/app/app.module.js
-
-# Ad textEditor
-bower install textAngular --save
-sed -i "s/angular-loading-bar/angular-loading-bar',\n\t\t\t'textAngular/" src/main/webapp/app/app.module.js
