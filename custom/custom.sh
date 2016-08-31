74ba3fb044072ac7bd141949ba846d04598b6a6e
Andres Fuentes <me@andresfuentes.com>
2016-08-31 20:29:42 +0000

    use jp JSON parser to apply features

--- custom/custom.sh
+++ custom/custom.sh
@@ -1,11 +1,18 @@
+#
+# Depends on jq
+#
+# sudo apt-get install jq.
 
-./features/application-settings/apply-feature.sh
-./features/css-theme/apply-feature.sh
-./features/dynamic-menu/apply-feature.sh
-./features/add-angular-tree/apply-feature.sh
-./features/load-initial-data/apply-feature.sh
-./features/checklist-tree/apply-feature.sh
-./features/audit-profile-tree/apply-feature.sh
+# ./features/application-settings/apply-feature.sh
+# ./features/css-theme/apply-feature.sh
+# ./features/dynamic-menu/apply-feature.sh
+# ./features/add-angular-tree/apply-feature.sh
+# ./features/load-initial-data/apply-feature.sh
+# ./features/checklist-tree/apply-feature.sh
+# ./features/audit-profile-tree/apply-feature.sh
+
+cat features.json |jq '.features[] | "./features/" + .path + "/" + .scripts.apply'|awk '{system(" chmod +x "$1)}'
+cat features.json |jq '.features[] | "./features/" + .path + "/" + .scripts.apply'|awk '{system($1)}'
 
 #
 # Update gulpfile
