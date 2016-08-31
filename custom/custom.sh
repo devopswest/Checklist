377472da99fa8995a3c5359f07a32608e1fa4cdb
Andres Fuentes <me@andresfuentes.com>
2016-08-31 14:18:37 +0000

    add liquibase scripts to load initial data

--- custom/custom.sh
+++ custom/custom.sh
@@ -102,6 +102,13 @@ cp -r custom/src/main/webapp/app/entities src/main/webapp/app
 # DB Updates
 #
 cp -r custom/src/main/resources/config/liquibase src/main/resources/config
+sed -i "s|<\/databaseChangeLog>| \
+\t<include file=\"classpath:config/liquibase/changelog/30000000000000_load_data.xml\" relativeToChangelogFile=\"false\"/> \
+\n</databaseChangeLog> \
+|" src/main/resources/config/liquibase/master.xml
+
+export PGPASSWORD='pwc123';
+psql -h adc-database.eastus.cloudapp.azure.com -p 5432 -U postgres
 
 #
 # Java Updates
