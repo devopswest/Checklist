4558ee1c18c40e97bbbb7e34342c40341097ea73
Andres Fuentes <me@andresfuentes.com>
2016-09-02 20:46:05 +0000

    add CSS Theme selector

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -54,6 +54,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/textAngular/dist/textAngular.js',
             'src/main/webapp/bower_components/textAngular/dist/textAngular-sanitize.js',
             'src/main/webapp/bower_components/textAngular/dist/textAngularSetup.js',
+            'src/main/webapp/bower_components/angular-object-diff/dist/angular-object-diff.js',
             'src/main/webapp/bower_components/angular-mocks/angular-mocks.js',
             // endbower
             'src/main/webapp/app/app.module.js',
