7dc670cefdf8f174b865613b346e8994be7313ca
Andres Fuentes <me@andresfuentes.com>
2016-09-02 00:25:30 +0000

    new feature

---
+++ custom/features/configure-git-ignore/apply-feature.sh
@@ -0,0 +1,8 @@
+echo "******************************"
+echo "**** CONFIGURE GIT IGNORE ****"
+echo "******************************"
+
+echo "######################" >> .gitignore
+echo "# Custom Settings     " >> .gitignore
+echo "######################" >> .gitignore
+echo "/src/main/webapp/bower_components/**" >> .gitignore
