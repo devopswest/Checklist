d7e7737aaa6b02c8baaa6ca57e2289885716d418
Andres Fuentes <me@andresfuentes.com>
2016-09-06 17:09:47 +0000

    add buttons at engagement level

---
+++ custom/features/development/add-engagement-actions/apply-feature.sh
@@ -0,0 +1,10 @@
+echo "**************************"
+echo "**** FEATURE [add-engagement-actions] ****"
+echo "**************************"
+
+
+
+#
+# Code updates
+#
+cp -r custom/features/$category/$feature/src ./
