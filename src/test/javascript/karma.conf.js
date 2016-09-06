09a37b7d0d9fc365c49fc59612106aba367830d9
Andres Fuentes <me@andresfuentes.com>
2016-09-06 13:08:31 +0000

    add ui.grid

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -57,6 +57,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/textAngular/dist/textAngularSetup.js',
             'src/main/webapp/bower_components/angular-object-diff/dist/angular-object-diff.js',
             'src/main/webapp/bower_components/ng-ckeditor/ng-ckeditor.js',
+            'src/main/webapp/bower_components/angular-ui-grid/ui-grid.js',
             'src/main/webapp/bower_components/angular-mocks/angular-mocks.js',
             // endbower
             'src/main/webapp/app/app.module.js',
