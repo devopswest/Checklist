2bc4e6ab02760455657e636a026432067ca6ea8c
Andres Fuentes <me@andresfuentes.com>
2016-09-07 04:43:17 +0000

    typo

--- custom/features/development/checklist-tree/apply-feature.sh
+++ custom/features/development/checklist-tree/apply-feature.sh
@@ -12,5 +12,5 @@ cp -r custom/features/development/checklist-tree/src/main src
 #
 # Update Code
 #
-sed -i "s|uses = {}|uses = {ChecklistQuestionMapper.class}|" src/main/java/com/pwc/assurance/adc/service/mapper/ChecklistDTO.java
+sed -i "s|uses = {}|uses = {ChecklistQuestionMapper.class}|" src/main/java/com/pwc/assurance/adc/service/mapper/ChecklistMapper.java
 
