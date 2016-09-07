0fe50a93a1deb759e4b8c02e5916fd1611183952
Andres Fuentes <me@andresfuentes.com>
2016-09-07 02:50:10 +0000

    add uses

--- custom/features/development/checklist-tree/apply-feature.sh
+++ custom/features/development/checklist-tree/apply-feature.sh
@@ -7,3 +7,10 @@ echo "************************"
 # Source Code  Updates
 #
 cp -r custom/features/development/checklist-tree/src/main src
+
+
+#
+# Update Code
+#
+sed -i "s|uses = {}|uses = {ChecklistQuestionMapper.class}|" src/main/java/com/pwc/assurance/adc/service/mapper/ChecklistDTO.java
+
