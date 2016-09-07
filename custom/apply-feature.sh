e2fa84a415117370a9d4be50b370c078779040ac
Andres Fuentes <me@andresfuentes.com>
2016-09-07 19:50:07 +0000

    typo

--- custom/apply-feature.sh
+++ custom/apply-feature.sh
@@ -2,7 +2,7 @@
 category="development"
 feature=$1
 
-chnmod +x ./custom/features/$category/$feature/apply-feature.sh
+chmod +x ./custom/features/$category/$feature/apply-feature.sh
 ./custom/features/$category/$feature/apply-feature.sh
 
 #gradle assemble
