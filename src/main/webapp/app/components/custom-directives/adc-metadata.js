6f6eceb8b39c4e9f6f66e9c90b3100c04d5aeaf2
Sukhpreet Singh <pwc@10.0.0.13>
2016-09-20 18:57:14 +0000

    Added controller and defined behavior at directive level.

--- src/main/webapp/app/components/custom-directives/adc-metadata.js
+++ src/main/webapp/app/components/custom-directives/adc-metadata.js
@@ -4,12 +4,22 @@
 	angular
     .module('checklistApp')
     .directive('adcMetadata', function () {
+    	
+    	var controller = ['$scope', function() {
+    		var vmDir = this;
+    		vmDir.toggle = function toggle(scope) {
+    			scope.toggle();
+    		};
+    	}];
+    	
     	return {
     		restrict: 'E',
     		scope: {
-    			metadata: '=treeData',
-    			toggle: '&onToggle'
+    			metadata: '=treeData'
     		},
+    		controller: controller,
+    		controllerAs: 'vmDir',
+    		bindToController: true,
     		templateUrl: function(element, attr) {
     			if(attr.type == 'metadata') {
     				return 'app/components/custom-directives/metadata-tree.html';
